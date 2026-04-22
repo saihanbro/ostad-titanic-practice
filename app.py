5b4efd7302258808c3a5222f58015a433de1c826
Abdullah Saihan <saihan0176@gmail.com>
2026-04-22 21:44:31 +0600

    change train test data ratio

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ y = df["Survived"]
 
 # 6. Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.1, random_state=42
 )
 
 
