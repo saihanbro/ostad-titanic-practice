82a481be0e3725462fddd070afe0c6728530c3a4
Abdullah Saihan <saihan0176@gmail.com>
2026-04-22 22:08:55 +0600

    typical branching workflow

--- new_feature.py
+++ new_feature.py
@@ -1,3 +1,15 @@
 """"
 add a new feature
+
+git branch new_feature_branc -> # create branch
+git checkout new_feature_branch -> # switch
+changes add+commit
+git checkout main # switch back to main
+if  new_feature_branch == "worked/ok"
+    git merge new_feature_branch
+
+
+single user -> same branch -> linear workflow
+single user -> multiple branch -> no linear workflow
 """
+
